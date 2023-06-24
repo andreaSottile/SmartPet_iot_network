0a91765f0dc624ff2b63ff68da7de9ec41868656
andre <a.sottile3@studenti.unipi.it>
2023-06-24 14:42:20 +0200

    Aggiunte delle cose fatte oggi

---
+++ Network_Controller/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Network_Controller.models import Window
+
+
+admin.site.register(Window)
---
+++ Network_Controller/models.py
@@ -0,0 +1,54 @@
+#https://docs.djangoproject.com/en/4.2/topics/db/models/
+
+from django.db import models
+
+
+class Food(models.Model):
+    lvl = models.IntegerField()
+    time = models.DateTimeField()
+    containerID = models.CharField(max_length=100)
+
+
+class FoodConfig(models.Model):
+    containerID = models.CharField(max_length=100)
+    lvlMax = models.IntegerField(default=1000)
+    lvlThreshold = models.IntegerField(default=300)
+    lvlMin = models.IntegerField(0)
+
+
+class Heartbeat(models.Model):
+    frequency = models.IntegerField()
+    time = models.DateTimeField()
+    petID = models.CharField(max_length=100)
+
+
+class HeartBeatConfig(models.Model):
+    petID = models.CharField(max_length=100)
+    max = 300
+    high_Threshold = models.IntegerField(default=200)
+    low_Threshold = models.IntegerField(default=40)
+    min = 0
+
+
+class Window(models.Model):
+    IN = 'INSIDE'
+    OUT = 'OUTSIDE'
+    DEF = 'DEFAULT'
+    time = models.DateTimeField()
+    windowID = models.CharField(max_length=100)
+    DIRECTION_TRIGGER_CHOICES = (
+        (IN, 'Inside'),
+        (OUT, 'Outside'),
+        (DEF, 'Default')
+    )
+    direction_Trigger = models.CharField(
+        choices=DIRECTION_TRIGGER_CHOICES,
+        default=DEF
+    )
+
+
+class WindowConfig(models.Model):
+    windowID = models.CharField(max_length=100)
+    allowOpen = models.BooleanField(default=False)
+
+
