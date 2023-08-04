ad00bdf93aa04aa002389d7709923a76610d5a2b
L.Nelli <52744702+B4dWeather@users.noreply.github.com>
2023-08-04 16:11:05 +0200

    bugfix

--- iot/COAP Client/COAP Food Refiller/coap-example-client.c
+++ iot/COAP Client/COAP Food Refiller/coap-example-client.c
@@ -40,12 +40,15 @@
 #include "random.h"
 #include "node-id.h"
 #include "os/dev/serial-line.h"
-#include "dev/etc/rgb-led/rgb-led.h"
 #include "dev/button-hal.h"
 
 /* Log configuration */
 #include "app_var.h"
 #include "coap-log.h"
+
+#include "dev/leds.h"
+#include "dev/rgb-led/rgb-led.h"
+
 #define LOG_MODULE "App"
 #define LOG_LEVEL  LOG_LEVEL_APP
 
@@ -53,53 +56,86 @@
 #define SERVER_EP "coap://[fd00::202:2:2:2]:5683"
 char *service_url = "/hello";
 
-bool node_registered = false;
+#define STATE_INIT            0
+#define STATE_REGISTERING      1
+#define STATE_REGISTERED       2
 int status = 0;
 char *res_name
 extern coap_resource_t res_status;
+/*---------------------------------------------------------------------------*/
+/* Led manipulation */
+#define RGB_LED_RED     1
+#define RGB_LED_GREEN   2
+#define RGB_LED_BLUE    4
+#define RGB_LED_MAGENTA (RGB_LED_RED | RGB_LED_BLUE)
+#define RGB_LED_YELLOW  (RGB_LED_RED | RGB_LED_GREEN)
+#define RGB_LED_CYAN    (RGB_LED_GREEN | RGB_LED_BLUE )
+#define RGB_LED_WHITE   (RGB_LED_RED | RGB_LED_GREEN | RGB_LED_BLUE)
 
-PROCESS(actuator_node, "actuator_node");
+void rgb_led_off(void) {
+    leds_off(LEDS_ALL);
+}
+
+void rgb_led_set(uint8_t colour) {
+    leds_mask_t leds =
+            ((colour & RGB_LED_RED) ? LEDS_RED : LEDS_COLOUR_NONE) |
+            ((colour & RGB_LED_GREEN) ? LEDS_GREEN : LEDS_COLOUR_NONE) |
+            ((colour & RGB_LED_BLUE) ? LEDS_BLUE : LEDS_COLOUR_NONE);
+
+    leds_off(LEDS_ALL);
+    leds_on(leds);
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS(actuator_node,
+"actuator_node");
 AUTOSTART_PROCESSES(&actuator_node);
 
 /* This function is will be passed to COAP_BLOCKING_REQUEST() to handle responses. */
-void client_chunk_handler(coap_message_t *response)
-{
-	const uint8_t *chunk;
+void client_chunk_handler(coap_message_t *response) {
+    const uint8_t *chunk;
 
-	if(response == NULL) {
-		LOG_INFO("Request timed out");
-		return;
-	}
-	node_registered = true;
-	int len = coap_get_payload(response, &chunk);
-	LOG_INFO("|%.*s \n", len, (char *)chunk);
+    if (response == NULL) {
+        LOG_INFO("Request timed out");
+        return;
+    }
+
+    int len = coap_get_payload(response, &chunk);
+    LOG_INFO("|%.*s \n", len, (char *) chunk);
+
+    if (status == STATE_REGISTERING)
+        status = STATE_REGISTERED;
 }
 
 
-PROCESS_THREAD(actuator_node, ev, data)
-{
-  static coap_endpoint_t serverCoap;
-  static coap_message_t request[1];
+PROCESS_THREAD(actuator_node, ev, data) {
+    static coap_endpoint_t serverCoap;
+    static coap_message_t request[1];
 
-  PROCESS_BEGIN();
-  LOG_INFO("Starting actuator node\n");
-  coap_activate_resource(&res_status, "food");
-  coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &serverCoap);
-  coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);  
-  coap_set_header_uri_path(request, service_url);
-  possible_ID= 1 + (int) random_rand() % 100;
-  const char msg[] = "food_" + possible_ID;
-  coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
-  COAP_BLOCKING_REQUEST(&serverCoap, request, client_chunk_handler);
-  LOG_INFO("--Node Registered--\n");
-  rgb_led_set(RGB_LED_GREEN);
+    PROCESS_BEGIN();
+    LOG_INFO("Starting actuator node\n");
+    coap_activate_resource(&res_status, "food");
+    coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &serverCoap);
+    coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+    coap_set_header_uri_path(request, service_url);
 
-  
-  while(1) {
-    PROCESS_WAIT_EVENT();
-     
-    //TODO GESTIONE CLIENT
-  }
+    rgb_led_set(RGB_LED_GREEN);
 
-  PROCESS_END();
+
+    while (1) {
+        if(status == STATE_INIT){
+            // In a real application, MAC address is to be used instead of random
+            possible_ID = 501 + (int) random_rand() % 500;
+            const char msg[] = "food_" + possible_ID;
+            status = STATE_REGISTERING;
+            coap_set_payload(request, (uint8_t *) msg, sizeof(msg) - 1);
+            COAP_BLOCKING_REQUEST(&serverCoap, request, client_chunk_handler);
+            LOG_INFO("--Node Registering--\n");
+        }
+        PROCESS_WAIT_EVENT();
+
+        //WORK PHASE: there is no actuator in the simulation
+    }
+
+    PROCESS_END();
 }
