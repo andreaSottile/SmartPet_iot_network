be2450b051c9316e74194c63ae2e557646705edc
L.Nelli <52744702+B4dWeather@users.noreply.github.com>
2023-09-05 19:05:59 +0200

    fixed mismatching DOM ids

--- static/js/script.js
+++ static/js/script.js
@@ -24,47 +24,56 @@ function sendPostRequest(inputId, url) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Aggiungere un listener per il click sul div con classe "button" dentro il div con id "set_top_food"
-    document.querySelector("#set_top_food .button").addEventListener("click", function () {
-        // Recuperare l'url dal data attribute del div
-        var url = this.getAttribute("data-url");
-        // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
 
-        var val = document.getElementById("threshold_top").value;
-        console.log(url+"/"+val)
-        sendPostRequest("threshold_top", url+"/"+val);
-    });
+    let top_food = document.getElementById("set_top_food");
+    if (top_food != null) {
+        top_food.addEventListener("click", function () {
+            // Recuperare l'url dal data attribute del div
+            var url = this.getAttribute("data-url");
+            // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
 
-    // Aggiungere un listener per il click sul div con classe "button" dentro il div con id "set_bottom_food"
-    document.querySelector("#set_bottom_food .button").addEventListener("click", function () {
-        // Recuperare l'url dal data attribute del div
-        var url = this.getAttribute("data-url");
-        // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
+            var val = document.getElementById("threshold_top").value;
+            console.log(url + "/" + val)
+            sendPostRequest("threshold_top", url + "/" + val);
+        });
+    }
 
-        var val = document.getElementById("threshold_bottom").value;
-        console.log(url+"/"+val)
-        sendPostRequest("threshold_bottom", url+"/"+val);
-    });
+    let bottom_food = document.getElementById("set_bottom_food");
+    if (bottom_food != null) {
+        bottom_food.addEventListener("click", function () {
+            // Recuperare l'url dal data attribute del div
+            var url = this.getAttribute("data-url");
+            // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
 
-    // Aggiungere un listener per il click sul div con classe "button" dentro il div con id "set_heartbeat_low"
-    document.querySelector("#set_heartbeat_low .button").addEventListener("click", function () {
-        // Recuperare l'url dal data attribute del div
-        var url = this.getAttribute("data-url");
-        // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
-          var val = document.getElementById("heartbeat_low").value;
-        console.log(url+"/"+val)
-        sendPostRequest("heartbeat_low", url+"/"+val);
-    });
+            var val = document.getElementById("threshold_bottom").value;
+            console.log(url + "/" + val)
+            sendPostRequest("threshold_bottom", url + "/" + val);
+        });
+    }
 
-    // Aggiungere un listener per il click sul div con classe "button" dentro il div con id "set_heartbeat_high"
-    document.querySelector("#set_heartbeat_high .button").addEventListener("click", function () {
-        // Recuperare l'url dal data attribute del div
-        var url = this.getAttribute("data-url");
-        // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
-          var val = document.getElementById("heartbeat_high").value;
-        console.log(url+"/"+val)
-        sendPostRequest("heartbeat_high", url+"/"+val);
-    });
+    let hearbeat_low = document.getElementById("set_heartbeat_low");
+    if (hearbeat_low != null) {
+        hearbeat_low.addEventListener("click", function () {
+            // Recuperare l'url dal data attribute del div
+            var url = this.getAttribute("data-url");
+            // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
+            var val = document.getElementById("heartbeat_low").value;
+            console.log(url + "/" + val)
+            sendPostRequest("heartbeat_low", url + "/" + val);
+        });
+    }
+
+    let heartbeat_high = document.getElementById("set_heartbeat_high");
+    if (heartbeat_high != null) {
+        heartbeat_high.addEventListener("click", function () {
+            // Recuperare l'url dal data attribute del div
+            var url = this.getAttribute("data-url");
+            // Invocare la funzione sendPostRequest con l'id dell'input e l'url recuperato
+            var val = document.getElementById("heartbeat_high").value;
+            console.log(url + "/" + val)
+            sendPostRequest("heartbeat_high", url + "/" + val);
+        });
+    }
 
 });
 
