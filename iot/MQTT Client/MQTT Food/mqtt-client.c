6601c0d612e543e3503052b2535f2652d43c510a
L.Nelli <52744702+B4dWeather@users.noreply.github.com>
2023-09-25 19:19:34 +0200

    Code cleanup 2

--- iot/MQTT Client/MQTT Food/mqtt-client.c
+++ iot/MQTT Client/MQTT Food/mqtt-client.c
@@ -299,7 +299,6 @@ PROCESS_THREAD(mqtt_client_process, ev, data) {
 		if (status_FoodTopic == MQTT_STATUS_OUT_QUEUE_FULL) {
 		  LOG_ERR("Tried to subscribe but command queue was full!\n");
 		    }
-		printf("%i \n", status_FoodTopic);
 		if (status_FoodTopic != 0) {
 			printf("reset timer for subscribe to topic actuator \n");
                 	etimer_set(&sub_timer, DEFAULT_PUBLISH_INTERVAL);
